c1fd89e60d4089844f72dbd927e945f02fa23edc
deadragons <48061756+deadragons@users.noreply.github.com>
2021-12-04 14:00:40 -0500

    Initial commit

---
+++ Assets/Codigos/LogicaPies.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPies : MonoBehaviour
+{
+    public LogicaPersonaje1 logicaPersonaje1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerStay( Collider other){
+        logicaPersonaje1.puedoSaltar = true;
+    }
+    public void OnTriggerExit( Collider other){
+        logicaPersonaje1.puedoSaltar = false;
+    }
+}
